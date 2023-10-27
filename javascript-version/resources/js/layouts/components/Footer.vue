27f803e520af5eb08434e53ebd6bc7ff23b982a1
Usman <memerboyemiology444@gmail.com>
2023-10-27 09:24:38 +0500

    first commit

---
+++ javascript-version/resources/js/layouts/components/Footer.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="h-100 d-flex align-center justify-space-between">
+    <!-- 👉 Footer: left content -->
+    <span class="d-flex align-center">
+      &copy;
+      {{ new Date().getFullYear() }}
+      Made With
+      <VIcon
+        icon="tabler-heart"
+        color="error"
+        size="1.25rem"
+        class="mx-1"
+      />
+     
+    </span>
+    <!-- 👉 Footer: right content -->
+    <span class="d-md-flex gap-x-4 text-primary d-none">
+      <a
+        href="#"
+        target="noopener noreferrer"
+      >License</a>
+     
+      <a
+        href="https://demos.pixinvent.com/vuexy-vuejs-admin-template/documentation/guide/laravel-integration/folder-structure.html"
+        target="noopener noreferrer"
+      >Documentation</a>
+    </span>
+  </div>
+</template>
